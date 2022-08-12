ce8759200b88da2e576f07c6a068914ec6055159
Gaurav konde <63660013+Gauravk2610@users.noreply.github.com>
2022-08-12 21:13:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { StaticData } from './staticData';
+import DisplayTable from './components/DisplayTable';
+import TestTable from './components/TestTable';
+// 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    // const response = await fetch('https://www.cryptingup.com/api/markets', {
+    //   method: 'GET',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   }
+    // })
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.cryptingup.com/api/markets", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+
+    // const response = await axios.get('https://www.cryptingup.com/api/markets');
+    // const data = response.data;
+    // console.log(data);
+  }
+  
+  useEffect(() => {
+    getData()
+
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TestTable />
+      <DisplayTable data={data} />
+
+      {
+        data.map((item, index) => {
+          return (
+            <div key={index}>
+              <h2>{item.symbol}</h2>
+              <h2>{item.price}</h2>
+              <h3>{item.last_price}</h3>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
---
+++ src/components/TestTable.js
@@ -0,0 +1,142 @@
+import React, { useEffect, useState } from 'react'
+// import styled from 'styled-components'
+import { useTable, useSortBy } from 'react-table'
+
+import { StaticData } from '../staticData';
+
+// const Styles = styled.div`
+//   padding: 1rem;
+
+//   table {
+//     border-spacing: 0;
+//     border: 1px solid black;
+
+//     tr {
+//       :last-child {
+//         td {
+//           border-bottom: 0;
+//         }
+//       }
+//     }
+
+//     th,
+//     td {
+//       margin: 0;
+//       padding: 0.5rem;
+//       border-bottom: 1px solid black;
+//       border-right: 1px solid black;
+
+//       :last-child {
+//         border-right: 0;
+//       }
+//     }
+//   }
+// `
+
+function Table({ columns, data }) {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useSortBy
+  )
+
+  // We don't want to render all 2000 rows for this example, so cap
+  // it at 20 for this use case
+  const firstPageRows = rows.slice(0, 20)
+
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                // Add the sorting props to control sorting. For this example
+                // we can add them into the header props
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  {/* Add a sort direction indicator */}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? ' 🔽'
+                        : ' 🔼'
+                      : ''}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {firstPageRows.map(
+            (row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    )
+                  })}
+                </tr>
+              )}
+          )}
+        </tbody>
+      </table>
+      <br />
+      <div>Showing the first 20 results of {rows.length} rows</div>
+    </>
+  )
+}
+
+function TestTable() {
+  const [data, setData] = useState([]);
+  
+  const columns = React.useMemo(
+    () => [
+
+      {
+        Header: 'Symbol',
+        accessor: 'symbol',
+      },
+      {
+        Header: 'Price',
+        accessor: 'price',
+      },
+    ],
+    []
+  )
+
+  useEffect(() => {
+    // getData();
+    // setInterval(() => {
+      const data = StaticData
+      const filteredData = data.filter(item => item.exchange_id === 'BINANCE' && item.quote_asset === 'USDT');
+      // filteredData.sort(function(a, b) {
+      //   return a.price - b.price;
+      // });
+      setData(data);
+    // // }, 5000);
+  })
+
+
+  // const data = StaticData
+
+  return (
+    // <Styles>
+    data &&
+      <Table columns={columns} data={data} />
+    // </Styles>
+  )
+}
+
+export default TestTable
